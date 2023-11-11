1746f3c450bd63459b15cf1f357969166b03062c
Michael Leighton <michael138leighton@gmail.com>
2023-11-11 11:42:01 -0500

    exercise 2 completed

---
+++ app.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('searchButton').addEventListener('click', function() {
+        fetch('superheroes.php')
+            .then(response => response.text())
+            .then(data => {
+                alert(data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    });
+});
